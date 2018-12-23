0ed4d6fdb1bdbcea782d1ecc6b520b874cab6949
aztran <andra.riztyan@gmail.com>
2018-12-23 12:33:35 +0700

    add docs folder

---
+++ docs/precache-manifest.bd3edd4b561bf7c01055b503936c31e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "861c854142760edd7ef1",
+    "url": "/js/chunk-vendors.fe106f66.js"
+  },
+  {
+    "revision": "e93981c7872721163b0d",
+    "url": "/js/app.1bb669dc.js"
+  },
+  {
+    "revision": "271aee0283865126a8faf810baccc085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "861c854142760edd7ef1",
+    "url": "/css/chunk-vendors.551fe22e.css"
+  },
+  {
+    "revision": "e93981c7872721163b0d",
+    "url": "/css/app.3b6a9b38.css"
+  }
+];
